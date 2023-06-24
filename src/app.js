5b67816a2b2a7901e028db4ff10500916d77acc3
Guede <Florencia.Guede@chubb.com>
2023-06-24 10:39:03 -0400

    crea carrito de compras

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-const express = require(`express`);
-const patch = require(`path`);
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5050;
 
-app.get(`/`, (req, res) => res.send(`Prueba`));
-app.listen(port, () => console.log(`Server Running`));
+app.get('/', (req, res) => res.send('Prueba'));
+
+
+app.get('/registration', (req, res) => {
+    let htmlPath = path.resolve(__dirname, '../views/registration.html');
+    res.sendFile(htmlPath);
+});
+
+app.get('/productCart', (req, res) => {
+    let htmlPath = path.resolve(__dirname, '../views/productCart.html');
+    res.sendFile(htmlPath);
+});
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.listen(port, () => console.log('Server Running'));
