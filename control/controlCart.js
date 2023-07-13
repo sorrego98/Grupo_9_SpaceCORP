2ddd80829c97f8f59e6f519e6bee4d4925ade0c5
Guede <Florencia.Guede@chubb.com>
2023-07-12 22:48:15 -0400

    update carrito ejs

--- control/controlCart.js
+++ control/controlCart.js
@@ -1,10 +1,9 @@
 const path = require("path");
 
 const controlCart = {
-    showCart : (req, res) => {
-        let htmlPath = path.resolve(__dirname, '../src/views/cart/product-cart.html');
-        res.sendFile(htmlPath);
-    }
+    showCart :(req, res) => {
+        res.render('./cart/product-cart');
+    },
 };
 
 module.exports = controlCart;
