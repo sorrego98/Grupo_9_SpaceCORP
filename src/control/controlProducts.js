9582b7f4389829466c3d888545d033904d2fd2c3
Carlos Echegaray <carlos.charlex@gmail.com>
2023-08-19 14:04:05 -0400

    modificación para listar productos

--- src/control/controlProducts.js
+++ src/control/controlProducts.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const controlMain = require('./controlMain');
 
-const controlProducts = {
-    products : (req, res) => {
-        res.render('./products/products')
-    }
+const controlProducts = {    
+    products: (req, res) => {
+        /*extraigo la lista de productos*/
+        const products = controlMain.controlMethods.leerJSON('products.json');  
+        /*los envío al front con la vista*/
+        res.render('./products/products', { products })
+    },
 };
 
 module.exports = controlProducts;
