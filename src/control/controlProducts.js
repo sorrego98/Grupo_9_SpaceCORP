3ad3a1ad0660aba009bd1521d5cc8cdb7d3ccaf9
sorrego98 <juan.sorrego98@gmail.com>
2023-10-21 08:39:40 -0500

    Cambios en la visualización del producto

--- src/control/controlProducts.js
+++ src/control/controlProducts.js
@@ -2,7 +2,7 @@ const path = require("path");
 const controlMain = require('./controlMain');
 const db = require('../database/models'); /*---> esto está tirando el proyecto*/
 
-const controlProducts = {    
+const controlProducts = {
     // productsDBJSON: function (req, res) {
     //     db.Category.findAll({include: [{association: 'subcategories'}]})
     //     .then( category => res.json(category))    
@@ -10,33 +10,35 @@ const controlProducts = {
     //     .catch(error => res.send("Error presente: " + error));
     // },
     products: function (req, res) {
-        db.Category.findAll({include: [{association: 'subcategories'}]})
-        .then(category => {
-            res.render('./products/products', { category })
-        })
-        .catch(error => res.send("Error presente: " + error));
+        db.Category.findAll({ include: [{ association: 'subcategories' }] })
+            .then(category => {
+                res.render('./products/products', { category })
+            })
+            .catch(error => res.send("Error presente: " + error));
     },
     detailProductsDBJSON: function (req, res) {
-        db.SubCategory.findByPk(req.params.id, {include: [{association: 'products'}]})//{include: [{association: 'subcategories'}]})
-        .then( subcategory => res.json(subcategory))    
+        db.SubCategory.findByPk(req.params.id, { include: [{ association: 'products' }] })//{include: [{association: 'subcategories'}]})
+            .then(subcategory => res.json(subcategory))
             /*res.render('./products/products', { products })*/
-        .catch(error => res.send("Error presente: " + error));
+            .catch(error => res.send("Error presente: " + error));
     },
     detailProducts: function (req, res) {
-        db.SubCategory.findByPk(req.params.id, {include: [{association: 'products'}]})//{include: [{association: 'subcategories'}]})
-        .then( subcategory => {
-            res.render('./products/detail-products', { subcategory })
-        })
-        .catch(error => res.send("Error presente: " + error));
+        const subcategories = db.SubCategory.findByPk(req.params.id, { include: [{ association: 'products' }] });//{include: [{association: 'subcategories'}]})
+        const productPrices = db.ProductPrice.findAll();
+        Promise.all([subcategories, productPrices])
+            .then(([allSubcategories, allProductPrices]) => {
+                res.render('./products/detail-products', { allSubcategories, allProductPrices })
+            })
+            .catch(error => res.send("Error presente: " + error));
     }
 };
 
-const controlProductsJSON = {    
+const controlProductsJSON = {
     products: (req, res) => {
         /*extraigo la lista de productos*/
-        const products = controlMain.controlMethods.leerJSON('products.json');  
+        const products = controlMain.controlMethods.leerJSON('products.json');
         /*los envío al front con la vista*/
-        
+
     },
 };
 
