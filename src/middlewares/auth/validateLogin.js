8be511606e8bac8e5a689ab11c15e853dd6c7a63
Carlos Echegaray <carlos.charlex@gmail.com>
2023-10-22 19:54:48 -0300

    cambios en el login/register

--- src/middlewares/auth/validateLogin.js
+++ src/middlewares/auth/validateLogin.js
@@ -1,8 +1,8 @@
 const { check } = require('express-validator');
 module.exports = validateLogin = [        
-        check('data').
+        check('userMailUserName').
             notEmpty().withMessage('Debes ingresar un email o tu username.'),
-        check('password').
+        check('userPass').
             notEmpty().withMessage('Debes ingresar una contraseña.').bail()
             .isLength({ min: 6 }).withMessage('Por regla, la contraseña debe tener 6 o más caracteres.')
     ];
