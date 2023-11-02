513373de16f8414defddc2635219c503dfc5ba37
sorrego98 <juan.sorrego98@gmail.com>
2023-11-02 11:18:20 -0500

    Instalación de CORS para el uso de API's en React

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+app.use(cors());
 
 //Requerir las rutas
 const routeMain = require("./routers/routesMain");
